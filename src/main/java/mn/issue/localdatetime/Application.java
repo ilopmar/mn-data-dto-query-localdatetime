ab4243ff15ae025593dbadaf18ffe1e1c2c2b241
Iván López <lopez.ivan@gmail.com>
2020-01-08 12:14:55 +0100

    Initial commit

---
+++ src/main/java/mn/issue/localdatetime/Application.java
@@ -0,0 +1,10 @@
+package mn.issue.localdatetime;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
